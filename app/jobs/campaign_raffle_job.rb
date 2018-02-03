042c49b497ded2ad0b612289126ab4bbe7322d90
Eduardo Pastre <dupastre@gmail.com>
2018-02-03 15:34:48 -0200

    removed comments from campaign_raffle_job

--- app/jobs/campaign_raffle_job.rb
+++ app/jobs/campaign_raffle_job.rb
@@ -2,16 +2,12 @@ class CampaignRaffleJob < ApplicationJob
   queue_as :email
 
   def perform(campaign)
-    puts "--- sasa"
     results = RaffleService.new(campaign).call
-    puts "\n\n ------- AQUI 1 \n\n"
     campaign.members.each {|m| m.set_pixel}
     results.each do |r|
       CampaignMailer.raffle(campaign, r.first, r.last).deliver_now
     end
-    puts "\n\n ------- AQUI \n\n"
     campaign.update(status: :finished)
-
 
     #if results == false
       # Send mail to owner of campaign (desafio)
